3c113eec4cc156d74b9d1fa2a08dbd3d34b8b173
zyrkeroleg <zyrker1@gmail.com>
2022-08-13 14:13:35 +0300

    настроил workflow

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", default='False')
 
 ALLOWED_HOSTS = ["*"]
 
@@ -72,11 +72,11 @@ WSGI_APPLICATION = "api_yamdb.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("DB_NAME", 'tour_DB_name'),
+        "USER": os.getenv("POSTGRES_USER", 'your_username'),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", 'your_password'),
+        "HOST": os.getenv("DB_HOST", 'your_host'),
+        "PORT": os.getenv("DB_PORT", 'port'),
     }
 }
 
